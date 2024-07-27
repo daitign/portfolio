c17a31dd213f5f23a2d5cd54fd5e62e471892b7e
Anonymanpro <blackhatzrg@gmail.com>
2024-07-27 20:32:40 +0800

    update
    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/synt.pro/",
+  base: "/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
